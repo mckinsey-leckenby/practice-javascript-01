237730b115c7e5375f6a29f4c80e76ff37e99db6
mckinseyleckenby <leckenbym11@gmail.com>
2022-03-14 12:10:19 -0700

    table2

--- table.js
+++ table.js
@@ -1,7 +1,8 @@
 const people = [
     { name: "John", age: 35},
     { name: "David", age: 27},
-    { name: "Nick", age: 41}
+    { name: "Nick", age: 41},
+    {name: "Chase", age: 43}
 ]
 
 console.table(people)
